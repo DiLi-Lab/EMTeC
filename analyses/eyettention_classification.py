6e2ea1cf6a1d3a92b3a31f7d39fb873e2edca9e3
Lena Bolliger <bolliger@tars.ifi.uzh.ch>
2024-05-05 11:43:06 +0200

    train part of train loop

--- analyses/eyettention_classification.py
+++ analyses/eyettention_classification.py
@@ -3,7 +3,16 @@
 Train the Eyettention architecture on a classification task of subjective text difficulty.
 """
 
-from analyses.utils.utils_analyses import prepare_eyettention_input, EMTeCDataset, calculate_mean_std
+from analyses.utils.utils_analyses import (
+    prepare_eyettention_input,
+    EMTeCDataset,
+    calculate_mean_std,
+    OrdinalHingeLoss,
+    get_kfold,
+    subset_data_kfold,
+    split_train_val,
+    gradient_clipping,
+)
 from transformers import BertTokenizerFast
 from argparse import ArgumentParser
 from analyses.utils.eyettention_model import Eyettention, ClassificationModel
@@ -11,6 +20,8 @@ from torch.utils.data import DataLoader
 import os
 import torch
 import torch.nn as nn
+import pandas as pd
+from collections import deque
 
 
 
@@ -43,6 +54,34 @@ def get_parser() -> ArgumentParser:
         choices=['last', 'all'],
         help='Whether to use all states or only the last state as input to the classification layers.',
     )
+    parser.add_argument(
+        '--loss',
+        type=str,
+        default='cross-entropy',
+        choices=['cross-entropy', 'ordinal-hinge', 'mse'],
+        help='The loss function to use',
+    )
+    parser.add_argument(
+        '--task',
+        type=str,
+        default='classification',
+        choices=['classification', 'regression'],
+        help='Whether to perform regression or classification.',
+    )
+    parser.add_argument(
+        '--target',
+        type=str,
+        default='difficulty',
+        choices=['difficulty', 'engaging'],
+        help='Whether we try to predict subjective text difficulty or engagement of the text.',
+    )
+    parser.add_argument(
+        '--split',
+        type=str,
+        default='subject',
+        choices=['subject', 'random'],
+        help='How to split the data for cross-validation.',
+    )
     return parser
 
 
@@ -61,6 +100,19 @@ def main():
     path_to_fixations = 'data/fixations_corrected.csv'
     path_to_ratings = 'data/participant_info/participant_results.csv'
     path_to_reading_measures = 'data/reading_measures_corrected.csv'
+    path_to_participant_info = 'data/participant_info/participant_info.csv'
+
+    # make sure the input arguments make sense
+    if args.task == 'classification':
+        n_targets = 5
+        if args.loss == 'mse':
+            raise RuntimeError('For classification, a categorical loss function should be used.')
+    elif args.task == 'regression':
+        n_targets = 1
+        if args.loss != 'mse':
+            raise RuntimeError('For regression, MSE should be used.')
+    else:
+        raise NotImplementedError(f'The task {args.task} is not implemented.')
 
     # config for training
     cf = {
@@ -81,7 +133,16 @@ def main():
         'classification_input': args.classification_input,
         'hidden_size': 128,
         'num_lstm_layers': 4,
+        'loss': args.loss,
+        'n_targets': n_targets,
+        'target': args.target,
+        'task': args.task,
+        'split': args.split,
     }
+
+    # # get a list of subject ids
+    # participant_info = pd.read_csv(path_to_participant_info, sep='\t')
+    # subject_ids = participant_info['subject_id'].tolist()
 
     tokenizer = BertTokenizerFast.from_pretrained(cf['model_pretrained'])
 
@@ -96,108 +157,200 @@ def main():
         max_sp_len=cf['max_sp_len'],
     )
 
-    emtec_dataset = EMTeCDataset(data)
-    emtec_dataloader = DataLoader(emtec_dataset, batch_size=cf['batch_size'], shuffle=True, drop_last=True)
-
-    # model = Eyettention(cf=cf)
-    # model.to(device)
-
-    # z-score normalization for gaze features
-    fix_dur_mean, fix_dur_std = calculate_mean_std(
-        dataloader=emtec_dataloader,
-        feat_key='sp_fix_dur',
-        padding_value=0,
-        scale=1000,
-    )
-    sn_word_len_mean, sn_word_len_std = calculate_mean_std(
-        dataloader=emtec_dataloader,
-        feat_key='sn_word_len',
-    )
-
-    model = ClassificationModel(
-        cf=cf,
-        device=device,
-    )
-    model.to(device)
-
-    optimizer = torch.optim.Adam(model.parameters(), lr=cf['lr'])
-
-    for batch_idx, batch in enumerate(emtec_dataloader):
-
-        sn_input_ids = batch['sn_input_ids'].to(device)
-        sn_attention_mask = batch['sn_attention_mask'].to(device)
-        word_ids_sn = batch['word_ids_sn'].to(device)
-        sn_word_len = batch['sn_word_len'].to(device)
-
-        sp_input_ids = batch['sp_input_ids'].to(device)
-        sp_attention_mask = batch['sp_attention_mask'].to(device)
-        word_ids_sp = batch['word_ids_sp'].to(device)
-        sp_len = batch['sp_len'].to(device)
-
-        sp_pos = batch['sp_pos'].to(device)
-        sp_fix_dur = (batch['sp_fix_dur'] / 1000).to(device)
-
-        # normalize gaze features
-        mask = ~torch.eq(sp_fix_dur, 0)
-        sp_fix_dur = (sp_fix_dur - fix_dur_mean) / fix_dur_std * mask
-        sp_fix_dur = torch.nan_to_num(sp_fix_dur)
-        sn_word_len = (sn_word_len - sn_word_len_mean) / sn_word_len_std
-        sn_word_len = torch.nan_to_num(sn_word_len)
-
-        labels = batch['rating_difficulty_one_hot'].to(device)
-
-        # fixation_representation = model(
-        #     sn_emd=sn_input_ids,
-        #     sn_mask=sn_attention_mask,
-        #     sp_emd=sp_input_ids,
-        #     sp_pos=sp_pos,
-        #     word_ids_sn=word_ids_sn,
-        #     word_ids_sp=word_ids_sp,
-        #     sp_fix_dur=sp_fix_dur,
-        #     sn_word_len=sn_word_len,
-        # )
 
 
-        out = model(
-            sn_input_ids=sn_input_ids,
-            sn_attention_mask=sn_attention_mask,
-            sp_input_ids=sp_input_ids,
-            sp_pos=sp_pos,
-            word_ids_sn=word_ids_sn,
-            word_ids_sp=word_ids_sp,
-            sp_fix_dur=sp_fix_dur,
-            sn_word_len=sn_word_len,
-            sp_len=sp_len,
+    # iterate through the folds for k-fold cross-validation
+    for train_idx, test_idx in get_kfold(
+        inputs=data['SN_input_ids'],
+        labels=data['ratings_difficulty'],
+        split=cf['split'],
+        n_splits=cf['n_folds'],
+        group=data['subject_ids'],
+    ):
+        # dict to hold the training progress
+        loss_dict = {
+            'train_loss': list(),
+            'val_loss': list(),
+            'test_ll': list(),
+            'test_AUC': list(),
+        }
+
+        # split the data into train and test fold
+        train_data, test_data = subset_data_kfold(data=data, train_idx=train_idx, test_idx=test_idx)
+
+        # split the train data into train and validation data
+        train_data, val_data = split_train_val(train_data=train_data)
+
+        # wrap in dataset class
+        train_dataset = EMTeCDataset(train_data)
+        test_dataset = EMTeCDataset(test_data)
+        val_dataset = EMTeCDataset(val_data)
+
+        # get dataloader iterator
+        train_dataloader = DataLoader(train_dataset, batch_size=cf['batch_size'], shuffle=True, drop_last=True)
+        test_dataloader = DataLoader(test_dataset, batch_size=cf['batch_size'], shuffle=False, drop_last=False)
+        val_dataloader = DataLoader(val_dataset, batch_size=cf['batch_size'], shuffle=False, drop_last=True)
+
+        # z-score normalization for gaze features (use the train data)
+        fix_dur_mean, fix_dur_std = calculate_mean_std(
+            dataloader=train_dataloader,
+            feat_key='sp_fix_dur',
+            padding_value=0,
+            scale=1000,
+        )
+        sn_word_len_mean, sn_word_len_std = calculate_mean_std(
+            dataloader=train_dataloader,
+            feat_key='sn_word_len',
         )
 
-        loss_fn = nn.CrossEntropyLoss()
-        loss = loss_fn(out, labels.float())
+        # load the model, the optimizer, and the loss function
 
-        print(loss)
+        model = ClassificationModel(
+            cf=cf,
+            device=device,
+        )
+        model.train()
+        model.to(device)
 
-        loss.backward()
-        optimizer.step()
+        optimizer = torch.optim.Adam(model.parameters(), lr=cf['lr'])
 
-        # lstm = nn.LSTM(
-        #     input_size=fixation_representation.size(-1),
-        #     hidden_size=cf['hidden_size'],
-        #     num_layers=4,
-        #     batch_first=True,
-        #     dropout=0.2,
-        #     bidirectional=True,
-        # )
-        # # h_n is of shape (num_layers * num_directions, batch, hidden_size)
-        # # it contains the final hidden state (concatenated, if bidirectional) for each LSTM layer
-        # lstm_out, (h_n, c_n) = lstm(fixation_representation)
-        # # access the last hidden state of the last layer for each element in the batch
-        # # we first need to reshape h_n to (num_layers, batch, num_directions * hidden_size)
-        #
-        # # Reshape to (num_layers, num_directions, batch, hidden_size)
-        # h_n = h_n.view(cf['num_lstm_layers'], 2, cf['batch_size'], cf['hidden_size'])
-        # # Swap dimensions to (num_layers, batch, num_directions, hidden_size)
-        # h_n = h_n.permute(0, 2, 1, 3)
-        # # Reshape to (num_layers, batch, 2 * hidden_size)
-        # h_n = h_n.contiguous().view(cf['num_lstm_layers'], cf['batch_size'], -1)
+        if cf['loss'] == 'cross-entropy':
+            loss_fn = nn.CrossEntropyLoss(reduction='mean')
+        elif cf['loss'] == 'ordinal-hinge':
+            loss_fn = OrdinalHingeLoss(num_classes=cf['n_targets'], device=device)
+        elif cf['loss'] == 'mse':
+            loss_fn = nn.MSELoss()
+
+        # variables for early stopping
+        old_score = 1e10
+        av_score = deque(maxlen=100)
+        save_ep_counter = 0
+
+        for epoch in range(cf['n_epochs']):
+            model.train()
+
+            for batch_idx, batch in enumerate(train_dataloader):
+
+                print(f'--- training epoch {epoch} batch {batch_idx}', end='\t')
+
+                sn_input_ids = batch['sn_input_ids'].to(device)
+                sn_attention_mask = batch['sn_attention_mask'].to(device)
+                word_ids_sn = batch['word_ids_sn'].to(device)
+                sn_word_len = batch['sn_word_len'].to(device)
+
+                sp_input_ids = batch['sp_input_ids'].to(device)
+                sp_attention_mask = batch['sp_attention_mask'].to(device)
+                word_ids_sp = batch['word_ids_sp'].to(device)
+                sp_len = batch['sp_len'].to(device)
+
+                sp_pos = batch['sp_pos'].to(device)
+                sp_fix_dur = (batch['sp_fix_dur'] / 1000).to(device)
+
+                # normalize gaze features
+                mask = ~torch.eq(sp_fix_dur, 0)
+                sp_fix_dur = (sp_fix_dur - fix_dur_mean) / fix_dur_std * mask
+                sp_fix_dur = torch.nan_to_num(sp_fix_dur)
+                sn_word_len = (sn_word_len - sn_word_len_mean) / sn_word_len_std
+                sn_word_len = torch.nan_to_num(sn_word_len)
+
+                if cf['target'] == 'difficulty':
+                    if cf['task'] == 'classification':
+                        labels = batch['rating_difficulty_one_hot'].to(device)
+                    elif cf['task'] == 'regression':
+                        labels = batch['rating_difficulty'].to(device)
+                    else:
+                        raise NotImplementedError
+                elif cf['target'] == 'engaging':
+                    if cf['task'] == 'classification':
+                        labels = batch['rating_engaging_one_hot'].to(device)
+                    elif cf['task'] == 'regression':
+                        labels = batch['rating_engaging'].to(device)
+                    else:
+                        raise NotImplementedError
+
+                # zero old gradients
+                optimizer.zero_grad()
+
+                # predict
+                out = model(
+                    sn_input_ids=sn_input_ids,
+                    sn_attention_mask=sn_attention_mask,
+                    sp_input_ids=sp_input_ids,
+                    sp_pos=sp_pos,
+                    word_ids_sn=word_ids_sn,
+                    word_ids_sp=word_ids_sp,
+                    sp_fix_dur=sp_fix_dur,
+                    sn_word_len=sn_word_len,
+                    sp_len=sp_len,
+                )
+
+                if args.loss == 'cross-entropy':
+                    loss = loss_fn(out, labels.float())
+                elif args.loss == 'ordinal-hinge':
+                    loss = loss_fn(out, labels)
+                elif args.loss == 'mse':
+                    loss = loss_fn(out.squeeze(), labels.float())
+
+                # backpropagate error
+                loss.backward()
+                # clip gradients
+                gradient_clipping(model, clip=cf['max_grad_norm'])
+                # learn
+                optimizer.step()
+
+                av_score.append(loss.to('cpu').detach().numpy())
+                print('\r error: {:.10f} '.format(loss.item()))
+
+            loss_dict['train_loss'].append(loss.to('cpu').detach().numpy())
+
+            val_loss = list()
+            model.eval()
+            for batch in val_dataloader:
+                print('--- validation ...')
+                with torch.no_grad():
+                    sn_input_ids = batch['sn_input_ids'].to(device)
+                    sn_attention_mask = batch['sn_attention_mask'].to(device)
+                    word_ids_sn = batch['word_ids_sn'].to(device)
+                    sn_word_len = batch['sn_word_len'].to(device)
+
+                    sp_input_ids = batch['sp_input_ids'].to(device)
+                    sp_attention_mask = batch['sp_attention_mask'].to(device)
+                    word_ids_sp = batch['word_ids_sp'].to(device)
+                    sp_len = batch['sp_len'].to(device)
+
+                    sp_pos = batch['sp_pos'].to(device)
+                    sp_fix_dur = (batch['sp_fix_dur'] / 1000).to(device)
+
+                    # normalize gaze features
+                    mask = ~torch.eq(sp_fix_dur, 0)
+                    sp_fix_dur = (sp_fix_dur - fix_dur_mean) / fix_dur_std * mask
+                    sp_fix_dur = torch.nan_to_num(sp_fix_dur)
+                    sn_word_len = (sn_word_len - sn_word_len_mean) / sn_word_len_std
+                    sn_word_len = torch.nan_to_num(sn_word_len)
+
+                    out = model(
+                        sn_input_ids=sn_input_ids,
+                        sn_attention_mask=sn_attention_mask,
+                        sp_input_ids=sp_input_ids,
+                        sp_pos=sp_pos,
+                        word_ids_sn=word_ids_sn,
+                        word_ids_sp=word_ids_sp,
+                        sp_fix_dur=sp_fix_dur,
+                        sn_word_len=sn_word_len,
+                        sp_len=sp_len,
+                    )
+                    if args.loss == 'cross-entropy':
+                        loss = loss_fn(out, labels.float())
+                    elif args.loss == 'ordinal-hinge':
+                        loss = loss_fn(out, labels)
+                    elif args.loss == 'mse':
+                        loss = loss_fn(out.squeeze(), labels.float())
+                    val_loss.append(loss.to('cpu').detach().numpy())
+
+
+
+
+
 
 
 
