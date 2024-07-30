18a77483193aa677720021a839f51305261244c3
Lena Bolliger <bolliger@tars.ifi.uzh.ch>
2024-07-30 11:22:33 +0200

    fix problems to download tensors

--- get_tensors.py
+++ get_tensors.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import requests
+import json
+import os
 
 
 def main():
@@ -8,29 +10,31 @@ def main():
     doi = 'https://doi.org/10.7910/DVN/GCU0W8'
 
     # Dataverse API endpoint
-    api_url = f'https://dataverse.harvard.edu/api/datasets/:persistentId/?persistentId=doi:{doi}'
+    #api_url = f'https://dataverse.harvard.edu/api/datasets/:persistentId/?persistentId=doi:{doi}'
+    api_url = 'https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/GCU0W8'
 
     # Make GET request to retrieve metadata
     response = requests.get(api_url)
 
-    # Check if request was successful
-    if response.status_code == 200:
-        # Parse JSON response
-        metadata = response.json()
+    with open('tensors/metadata.json', 'r') as f:
+        metadata = json.load(f)
 
-        # Extract download links for files
-        files = metadata['data']['latestVersion']['files']
+    if response.status_code == 200:
+        files = metadata['datasetVersion']['files']
         for file_info in files:
             file_id = file_info['dataFile']['id']
             download_url = f'https://dataverse.harvard.edu/api/access/datafile/{file_id}'
-
             # Download file
+            breakpoint()
             file_response = requests.get(download_url)
             # Save file to disk or process it as needed
-            with open(file_info['dataFile']['filename'], 'wb') as f:
+            with open(os.path.join('tensors', file_info['dataFile']['filename']), 'wb') as f:
                 f.write(file_response.content)
+
     else:
         print('Failed to retrieve dataset metadata')
+
+
 
 if __name__ == '__main__':
     raise SystemExit(main())
